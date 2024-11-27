cc3c32b6c9742078dccc0f88d7c76dd9c49b3b7e
mallelli88 <laxminryn88@gmail.com>
2024-11-27 00:03:11 -0800

    first commit

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_providers {
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = "=4.1.0"
+      }
+    }
+  
+}
+provider "azurerm" {
+    features {
+      
+    }
+    subscription_id  = "854d17e4-ca3f-4b1e-96e2-cfc7efeed3b2"
+    client_id        = "9244e98d-44b8-4a33-b993-1c8b3789c5a6"
+    client_secret    = "bpN8Q~EZz3te7._5BGRmXe1H1akt4QKBJxEgrbCw"
+    tenant_id        = "87d6bd65-e188-44fc-8e13-1a96ea0080df"
+  
+}
+
